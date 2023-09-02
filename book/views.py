6a274a2840d01e5665c3492b2d37e84baf1ea185
theMorshed <themorshedctg@gmail.com>
2023-09-02 14:12:47 +0600

    route secured and borrowlist, wishlist unique item add functionality

--- book/views.py
+++ book/views.py
@@ -13,30 +13,46 @@ def home(request):
     return render(request, 'books.html', context)
 
 def add_wishlist(request, book_id):
-    book = Book.objects.get(id = book_id)
-    item = WishList.objects.create(
-        user = request.user,
-        item = book,
-    )
-    item.save()
+    if not request.user.is_authenticated:
+        return redirect('login')
     
-    return redirect('home')
+    book = Book.objects.get(id = book_id)
+    is_exists = WishList.objects.filter(item = book)
+    if not is_exists:
+        item = WishList.objects.create(
+            user = request.user,
+            item = book,
+        )
+        item.save()
+    
+    return redirect('wishlist')
 
 def wishlist(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    
     wishlist = WishList.objects.filter(user = request.user)
     return render(request, 'wishlist.html', {'books': wishlist})
 
 def add_borrowlist(request, book_id):
-    book = Book.objects.get(id = book_id)
-    item = BorrowList.objects.create(
-        user = request.user,
-        item = book,
-        borrow_date = datetime.now(),
-    )
-    item.save()
+    if not request.user.is_authenticated:
+        return redirect('login')
     
-    return redirect('home')
+    book = Book.objects.get(id = book_id)
+    is_exists = BorrowList.objects.filter(item = book)
+    if not is_exists:
+        item = BorrowList.objects.create(
+            user = request.user,
+            item = book,
+            borrow_date = datetime.now(),
+        )
+        item.save()
+    
+    return redirect('borrowlist')
 
 def borrowlist(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    
     borrowlist = BorrowList.objects.filter(user = request.user)
     return render(request, 'borrowlist.html', {'books': borrowlist})
